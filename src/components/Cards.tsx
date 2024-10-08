2838b82ff7abc15d9f6194176c8a617346738b78
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2024-10-08 18:00:06 +0100

    fix bug

---
+++ src/components/Cards.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Cards = () => {
+  return (
+    <div className="flex flex-col gap-1 p-2">
+      <Image src="" alt="" />
+    </div>
+  );
+};
+
+export default Cards;
--- src/components/Tournaments.tsx
+++ src/components/Tournaments.tsx
@@ -1,16 +1,42 @@
+import Image from "next/image";
 import React from "react";
+import Cards from "./Cards";
 
 const Tournaments = () => {
   return (
     <div className="mt-4 flex flex-col gap-4">
       {/* TOP */}
-      <div className="">
+      <div className="flex items-center justify-between p-2">
         <h3 className="font-bold text-xl">Tournaments</h3>
-        <div className=""></div>
+
+        <div className="hidden md:flex items-center justify-center gap-2">
+          <div className="flex items-center justify-between w-[138px] h-[40px] ">
+            <Image src="/Sort.png" alt="" width={30} height={30} />
+            <p className="text-sm">Sort by</p>
+            <Image
+              src="/Down.png"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+          </div>
+          <div className="flex items-center justify-between w-[138px] h-[40px] ">
+            <Image src="/Filter.png" alt="" width={30} height={30} />
+            <p className="text-sm">Filter</p>
+            <Image
+              src="/Down.png"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
       </div>
 
       {/* CARDS */}
-      <div className=""></div>
+      <Cards />
     </div>
   );
 };
